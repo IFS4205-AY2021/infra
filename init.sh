1203afdfc00e36c9d50e4b18156a69b2c4a2e88a
jingchen-z <z782180503@gmail.com>
2020-09-11 04:36:27 +0800

    Updated compose file with backend & frontend service

--- init.sh
+++ init.sh
@@ -2,7 +2,22 @@
 
 source .env
 export COMPOSE_PROJECT_NAME=ifs
-export BASEDIR=$(dirname "$0")
-# mkdir $BASEDIR/$MYSQL_MAIN_DATA
-# mkdir $BASEDIR/$MYSQL_SUB_DATA
+export BASEDIR=$(pwd)
+echo "Using $BASEDIR as work directory."
+
+export MYSQL_MAIN_DATA=$(echo $BASEDIR/data/db_main)
+export MYSQL_SUB_DATA=$(echo $BASEDIR/data/db_sub)
+mkdir $BASEDIR/data
+mkdir $MYSQL_MAIN_DATA
+mkdir $MYSQL_SUB_DATA
+
+sed -i "/MYSQL_MAIN_DATA/c MYSQL_MAIN_DATA=$MYSQL_MAIN_DATA" .env
+sed -i "/MYSQL_SUB_DATA/c MYSQL_SUB_DATA=$MYSQL_SUB_DATA" .env
+
+
+
+# docker build ../backend-admin
+# docker build ../backend-researcher
+# docker build ../backend-tracer
+# docker build ../frontend
 
